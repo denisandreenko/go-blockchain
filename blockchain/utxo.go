e51513f88adf61d6be2d6f39d503cd9d2a67bb91
Dzianis Andreyenka <andreenkodn@gmail.com>
2022-06-14 09:11:10 +0200

    add finding unspent txs

--- blockchain/utxo.go
+++ blockchain/utxo.go
@@ -1,6 +1,7 @@
 package blockchain
 
 import (
+	"encoding/hex"
 	"github.com/dgraph-io/badger"
 	"log"
 )
@@ -12,6 +13,62 @@ var (
 
 type UTXOSet struct {
 	Blockchain *Blockchain
+}
+
+func (u *UTXOSet) FindUnspentTransactions(pubKeyHash []byte) []TxOutput {
+	var UTXOs []TxOutput
+
+	db := u.Blockchain.Database
+
+	err := db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+
+		it := txn.NewIterator(opts)
+		defer it.Close()
+
+		for it.Seek(utxoPrefix); it.ValidForPrefix(utxoPrefix); it.Next() {
+			item := it.Item()
+			var v []byte
+			err := item.Value(func(val []byte) error {
+				v = append([]byte{}, val...)
+				return nil
+			})
+			Handle(err)
+			outs := DeserializeOutputs(v)
+			for _, out := range outs.Outputs {
+				if out.IsLockedWithKey(pubKeyHash) {
+					UTXOs = append(UTXOs, out)
+				}
+			}
+
+		}
+		return nil
+	})
+	Handle(err)
+
+	return UTXOs
+}
+
+func (u *UTXOSet) Reindex() {
+	db := u.Blockchain.Database
+
+	u.DeleteByPrefix(utxoPrefix)
+
+	UTXO := u.Blockchain.FindUTXO()
+
+	err := db.Update(func(txn *badger.Txn) error {
+		for txId, outs := range UTXO {
+			key, err := hex.DecodeString(txId)
+			Handle(err)
+			key = append(utxoPrefix, key...)
+
+			err = txn.Set(key, outs.Serialize())
+			Handle(err)
+		}
+
+		return nil
+	})
+	Handle(err)
 }
 
 func (u *UTXOSet) DeleteByPrefix(prefix []byte) {
