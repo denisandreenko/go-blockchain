523d8bec135b9ee77bbeac78c98e8f834f410de7
Dzianis Andreyenka <andreenkodn@gmail.com>
2022-07-08 10:43:06 +0200

    update send fn

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -38,13 +38,10 @@ func CoinbaseTx(to, data string) *Transaction {
 	return &tx
 }
 
-func NewTransaction(from, to string, amount int, UTXO *UTXOSet) *Transaction {
+func NewTransaction(w *wallet.Wallet, to string, amount int, UTXO *UTXOSet) *Transaction {
 	var inputs []TxInput
 	var outputs []TxOutput
 
-	wallets, err := wallet.CreateWallets()
-	Handle(err)
-	w := wallets.GetWallet(from)
 	pubKeyHash := wallet.PublicKeyHash(w.PublicKey)
 	acc, validOutputs := UTXO.FindSpendableOutputs(pubKeyHash, amount)
 
@@ -62,6 +59,7 @@ func NewTransaction(from, to string, amount int, UTXO *UTXOSet) *Transaction {
 		}
 	}
 
+	from := fmt.Sprintf("%s", w.Address())
 	outputs = append(outputs, *NewTxOutput(amount, to))
 
 	if acc > amount {
